583d39451b059b01245bcc33fb2efa27abae650e
khoirul anam <khoirul.20998@gmail.com>
2018-12-19 16:35:16 +0700

    Buat Folder baru dengan nama latihan di kerjakan pada tanggal 19 Desember 2018

---
+++ latihan/bootstrap-4/assets/custom_js/apawe.js
@@ -0,0 +1,32 @@
+$(() => {
+    $('[data-toggle="tooltip"]').tooltip();
+    $(".linknya, footer a[href='#myPage']").on('click', function(event) {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 900, function() {
+            window.location.hash = hash;
+        });
+    });
+});
+var readURL = (event, elementNya, classnya) => {
+    $(classnya).slideUp('slow');
+    $(classnya + '-text').text('');
+    if (event.target.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function() {
+            var output = document.getElementById(elementNya);
+            output.src = reader.result;
+        };
+        reader.readAsDataURL(event.target.files[0]);
+        $(classnya).slideDown('slow');
+    } else {
+        $(classnya).slideUp('slow');
+        $(classnya + '-text').text('');
+    }
+    setTimeout((e) => {
+        $(classnya).slideUp('slow');
+        $(classnya + '-text').text('Sudah input Photo');
+    }, 10000)
+}
